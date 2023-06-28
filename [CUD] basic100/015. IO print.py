dc21c9f4ba4a46c9edca02ecf0941f9d63724851
Sangmin Park <parksm0529@gmail.com>
2023-06-28 14:15:47 +0900

    https://codeup.kr/problem.php?id=6015 | 3 min

---
+++ [CUD] basic100/015. IO print.py
@@ -0,0 +1,10 @@
+# 공백을 가진 두개의 정수 입력
+
+# num1, num2 = map(int, input().split())
+# print(f"{num1}\n{num2}")
+# 코드 길이:64 byte(s) / 수행 시간:0 ms / 메모리 :14316 kb
+
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+print(f"{num1}\n{num2}")
